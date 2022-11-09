476656d364bd05d315e3be2646e4391acfa745a6
Erik Hennerfors <erik.hennerfors@laget.se>
2022-11-09 11:11:08 +0100

    first commit

---
+++ Cronitor.Extensions.Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace StatsdClient.Extensions.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseCronitor(this IHostBuilder builder, string apiKey)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            ConfigureCronitor(apiKey);
+
+            return builder;
+        }
+
+        private static void ConfigureCronitor(string apiKey)
+        {
+            if (string.IsNullOrWhiteSpace(apiKey)) throw new ArgumentNullException(nameof(apiKey));
+
+            Cronitor.Cronitor.Configure(apiKey);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# Cronitor.Extensions.Hosting
+Hosting and startup abstractions for Cronitor. When using NuGet 3.x this package requires at least version 3.4....
+
+## Usage
+```c#
+await Host.CreateDefaultBuilder()
+    .UseCronitor("apiKey")
+    .Build()
+    .RunAsync();
+```
