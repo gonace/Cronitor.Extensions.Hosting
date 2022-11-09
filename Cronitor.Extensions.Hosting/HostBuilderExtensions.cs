d7f385de6214968b5976c5a45381d73f25e60146
Erik Hennerfors <erik.hennerfors@laget.se>
2022-11-09 12:20:38 +0100

    Updated HostBuilderExtensions

--- Cronitor.Extensions.Hosting/HostBuilderExtensions.cs
+++ Cronitor.Extensions.Hosting/HostBuilderExtensions.cs
@@ -14,6 +14,19 @@ namespace Cronitor.Extensions.Hosting
             return builder;
         }
 
+        public static IHostBuilder UseCronitor(this IHostBuilder builder, Func<HostBuilderContext, string> options)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.ConfigureServices((context, services) =>
+            {
+                var apiKey = options(context);
+                ConfigureCronitor(apiKey);
+            });
+
+            return builder;
+        }
+
         private static void ConfigureCronitor(string apiKey)
         {
             if (string.IsNullOrWhiteSpace(apiKey)) throw new ArgumentNullException(nameof(apiKey));
